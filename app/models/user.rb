3a9c630fee3b79d022e217c9a8fb0049e276d7e4
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2014-01-28 20:34:53 -0700

    Add migration to include token and secret for fitbit

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,17 @@ class User < ActiveRecord::Base
     create! do |user|
       user.provider = auth["provider"]
       user.uid = auth["uid"]
-      user.name = auth["info"]["nickname"]
+      user.name = auth["info"]["name"]
+      user.token = auth["extra"]["access_token"].token
+      user.secret = auth["extra"]["access_token"].secret
     end
   end
 
+  def fitbit_stats
+    # data = UserFitbitData.new(user)
+    # data.sleep(date)
+    # client = Fitgem::Client.new({:consumer_key => 'ee59ac70ceb04f2c90fb103d3f0ac969', :consumer_secret => '06ea4da5140e4a3e9ae1524d5f047ba4', :token => token, :secret => secret, :user_id => uid})
+    FitbitStats.new(self)
+    # p client.sleep_on_date('today')["summary"]["totalMinutesAsleep"]
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140128213702) do
+ActiveRecord::Schema.define(version: 20140129022757) do
 
   create_table "users", force: true do |t|
     t.string   "provider"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20140128213702) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "token"
+    t.string   "secret"
   end
 
 end
