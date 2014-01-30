12f2acb94e5368b956fe4806c2dfae22e507a504
Bryana Knight <brknig11@gmail.com>
2014-01-30 10:19:05 -0700

    delete duplicate root_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 ProjectX::Application.routes.draw do
 
-  root to: 'dashboard#index'
-
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
