4397ce697e0381b53860e138802b9c8352c9666f
Bryana Knight <brknig11@gmail.com>
2014-02-11 16:58:22 -0700

    take out localhost for faraday

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -11,19 +11,11 @@ class Activity
       "date" => date
     }}.to_query
     connection = Faraday.new
-    response = connection.get "#{fitbit_service_uri}/api/v1/activity?#{params}"
+    response = connection.get "http://fitbit-service.herokuapp.com/api/v1/activity?#{params}"
     data = JSON.parse(response.body)
     stat = user.stats.find_or_create_by(date: date)
     stat.update(steps: data["steps_on_date"], sleep: data["sleep_on_date"])
     stat
-  end
-
-  def self.fitbit_service_uri
-    if Rails.env == "production"
-      "http://fitbit-service.herokuapp.com"
-    else
-      "http://localhost:9292"
-    end
   end
 
 end
